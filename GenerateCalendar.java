34bbc3f4e9ae32b1c5126ea20019a64ab75a8b7b
Sortinn <sortinndecar@gmail.com>
2017-09-23 11:45:46 +0800

    extract some constants

--- GenerateCalendar.java
+++ GenerateCalendar.java
@@ -3,16 +3,17 @@ import java.util.*;
 
 public class GenerateCalendar {
     private Map<Integer, Integer> monthAndDay = new LinkedHashMap<>();
-    private final int day = 29;
-    private int month;
+    private final int DAY_28 = 28;
+    private final int DAY_29 = 29;
+    private final int DAY_30 = 30;
+    private final int DAY_31 = 31;
+
     private String week = "日" + "\t" + "一" + "\t" + "二" + "\t" + "三" + "\t" + "四" + "\t" + "五" + "\t" + "六" + "\t";
-    private int record;
 
 
     public int findFirstDay(int month, int year) {
         CalculateWeek cal = new CalculateWeek();
-        int fisrtDay = cal.week(1, month, year);    //查询该年每月第一天在星期几
-        return fisrtDay;
+        return cal.week(1, month, year);
 
     }
 
@@ -21,12 +22,13 @@ public class GenerateCalendar {
     }
 
     public int days(int month) {
+        MyCalendar ca = new MyCalendar();
         if (month == 2) {
-            return day;
+            return DAY_29;
         } else if (judgeMonth_30(month)) {
-            return day + 1;
+            return DAY_30;
         }
-        return day + 2;
+        return DAY_31;
     }
 
     public void formatMap() {
@@ -49,16 +51,16 @@ public class GenerateCalendar {
             }
 
             if (judgeMonth_30(entry.getKey())) {
-                printDays(findFirstDay(entry.getKey(), year),30);
+                printDays(findFirstDay(entry.getKey(), year), DAY_30);
                 System.out.println();
             } else if (entry.getKey() == 2) {
                 if (my.isLeapYear(year)) {
-                    printDays(findFirstDay(entry.getKey(), year),29);
-                }else printDays(findFirstDay(entry.getKey(), year),28);
+                    printDays(findFirstDay(entry.getKey(), year), DAY_29);
+                } else printDays(findFirstDay(entry.getKey(), year), DAY_28);
 
                 System.out.println();
             } else {
-                printDays(findFirstDay(entry.getKey(), year), 31);
+                printDays(findFirstDay(entry.getKey(), year), DAY_31);
                 System.out.println();
             }
         }
@@ -77,13 +79,12 @@ public class GenerateCalendar {
     }
 
 
-    public static void main(String[] args) {
-        System.out.println("以下是结果");
-        GenerateCalendar gene = new GenerateCalendar(2001);
+//    public static void main(String[] args) {
+//        System.out.println("以下是结果");
+//        GenerateCalendar gene = new GenerateCalendar(2001);
 //        for (int value : gene.monthAndDay.keySet()) {
 //            System.out.println(value);
 //            System.out.println(gene.monthAndDay.get(value) + "天");
 //        }
-
-    }
+//    }
 }
